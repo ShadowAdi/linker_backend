f5cf4b699eb59fb1ea39391a26921c11c9b91132
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-22 16:36:44 +0530

    Connected controller  to router

--- src/routes/linkRouter.js
+++ src/routes/linkRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { GetAllLinks } from "../controllers/linkController.js";
+import { CreateLink, GetAllLinks } from "../controllers/linkController.js";
 import { CheckAuth } from "../middleware/AuthCheck.js";
 
 export const linkRouter = express.Router();
 
 
 linkRouter.get("/",CheckAuth,GetAllLinks)
-linkRouter.post("/",CheckAuth,GetAllLinks)
+linkRouter.post("/",CheckAuth,CreateLink)
