3fbb36856539c9aeeefa2f713226b693d604de02
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-22 16:14:28 +0530

    Remove abstraction

--- src/controllers/linkController.js
+++ src/controllers/linkController.js
@@ -1,9 +1,45 @@
-import { prismaClient } from "../db/prisma";
-import { CustomTryCatch } from "../utils/CustomTryCatch";
-import { UserCheckAbstraction } from "../utils/userAbstracted";
+import { prismaClient } from "../db/prisma.js";
+import { AppError } from "../utils/AppError.js";
+import { CustomTryCatch } from "../utils/CustomTryCatch.js";
+import { logger } from "../utils/logger.js";
 
 export const GetAllLinks = CustomTryCatch(async (req, res, next) => {
-  const userFound = await UserCheckAbstraction(req,next);
+  const user = req.user;
+  if (!user) {
+    logger.error(`Failed to get the authenticated user ${user}`);
+    console.log(`Failed to get the authenticated user ${user}`);
+    return next(
+      new AppError(`Failed to get the authenticated user ${user}`, 404)
+    );
+  }
+  const { email, sub } = user;
+  if (!sub) {
+    logger.error(`Failed to get the authenticated user ${sub}`);
+    console.log(`Failed to get the authenticated user ${sub}`);
+    return next(
+      new AppError(`Failed to get the authenticated user ${sub}`, 404)
+    );
+  }
+  const userFound = await prismaClient.user.findUnique({
+    where: { id: sub },
+    select: {
+      email: true,
+      createdAt: true,
+      id: true,
+      name: true,
+      Links: true,
+    },
+  });
+  if (!userFound) {
+    logger.error(`User With Id Do Not Exist: ${sub}`);
+    console.log(`User With Id Do Not Exist: ${sub}`);
+    return next(new AppError(`User With Id Do Not Exist: ${sub}`, 404));
+  }
+  if (userFound.email !== email) {
+    logger.error(`User With email Do Not Exist: ${email}`);
+    console.log(`User With email Do Not Exist: ${email}`);
+    return next(new AppError(`User With email Do Not Exist: ${email}`, 404));
+  }
   const userLinks = await prismaClient.links.findMany({
     where: { userId: userFound.id },
   });
@@ -12,4 +48,3 @@ export const GetAllLinks = CustomTryCatch(async (req, res, next) => {
     userLinks,
   });
 });
-
--- src/routes/linkRouter.js
+++ src/routes/linkRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { GetAllLinks } from "../controllers/linkController";
+import { GetAllLinks } from "../controllers/linkController.js";
+import { CheckAuth } from "../middleware/AuthCheck.js";
 
 export const linkRouter = express.Router();
 
 
-linkRouter.get("/",GetAllLinks)
+linkRouter.get("/",CheckAuth,GetAllLinks)
