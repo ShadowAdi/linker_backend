269e5047b9eb8abea05460da643c35d6e12ad5d4
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-28 14:01:01 +0530

    Change the url

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://linker-frontend-ten.vercel.app/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
